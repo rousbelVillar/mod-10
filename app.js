c49c408fb16fae6782898663ba8cb0d28e0f5a99
Rousbel Villar <rousbelvillar@Rousbels-MacBook-Air.local>
2020-12-09 19:26:40 -0400

    uploading app

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors());
+app.get('/practica-9/modulo10/ingreseCedula/:cedula', (req, res) => {
+  res.status(200).send(showAnswer(req.params.cedula))
+})
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port ,()=>{
+    console.log("listening to port:" + port)
+});
+
+function getIsCedulaValid(cedula){
+    const Adasd = 0;
+    const cedulaArray = Array.from(cedula).map(Number);
+    let total = 0;
+    let i;
+    for(i = 0; i < cedulaArray.length; i++){
+        if(i  % 2){
+            cedulaArray[i] = cedulaArray[i] * 2;
+            cedulaArray[i] = cedulaArray[i] > 9 ? getElementSum(cedulaArray[i]) : cedulaArray[i];
+         }
+    }
+    cedulaArray.forEach(digit=>{
+        total = total + digit;
+    });
+    return +total.toString().split('').pop() === 0 ?  true : false;
+}
+
+function getElementSum(cedulaDigit){
+    const sumOfDigits = Array.from(cedulaDigit.toString()).map(Number);
+    return sumOfDigits[0] + sumOfDigits[1];
+}
+
+function showAnswer(cedula){
+    const correctLength = cedula.length === 11 && cedula != 0000000000;
+    const isCedulaValid = getIsCedulaValid(cedula);
+    const confirmationMessage = isCedulaValid ? 'correcta' : 'incorrecta';
+    const responseMessage = correctLength ?  'Su cedula: ' + cedula + ' es '  + confirmationMessage: 
+                                             'La cedula debe contener 11 digitos y no puede tener solo ceros';
+    return responseMessage;
+}
